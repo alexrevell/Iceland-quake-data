6d08ae7973f7d116aa0515831ee6e6dd0cec1228
alexrevell <revellalex@gmail.com>
2015-06-01 17:43:05 +1200

    get data to d3 to display initially on page in individual basis

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,11 +1,32 @@
 
 (function(){
-  var baseURI = "http://apis.is/";
-  var getQuakesURI = "earthquake/is";
-  d3.json("http://apis.is/earthquake/is", renderQuakes)
+  var quakesURL = "http://apis.is/earthquake/is";
+  getAndExtractQuakesEventHandler(quakesURL, getQuakes, extractQuakes, renderQuakes)
+
 })();
 
-function renderQuakes(quakeResults){
-  // quakeResults.forEach(function(index, quake){
-    console.log(quakeResults);
+function getAndExtractQuakesEventHandler(url, getQuakes, extractQuakes, renderQuakes){
+  getQuakes(url, function(data){
+    var quakes = extractQuakes(data);
+    renderQuakes(quakes)
+  })
+}
+
+function getQuakes(url, callback){
+  d3.json(url, callback);
+}
+
+function extractQuakes(quakeResults){
+  console.log(quakeResults.results)
+  return quakeResults.results
+}
+
+function renderQuakes(quakes){
+  // quakes.forEach(function(index, quake){
+    d3.select('.quake-events').selectAll('p')
+      .data(quakes)
+      .enter()
+      .append('p')
+      .text(function(d){return d});
+  // })
 }
