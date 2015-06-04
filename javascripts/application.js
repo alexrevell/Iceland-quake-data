4a20a70756bbe3a20e701eff09f3b6bacac058e5
alexrevell <revellalex@gmail.com>
2015-06-04 21:14:26 +1200

    small refactor - gotta keep em indenting right

--- javascripts/application.js
+++ javascripts/application.js
@@ -70,90 +70,90 @@ function renderQuakes(quakes){
         });
 
     svg.selectAll('text')
-        .data(realQuakes)
-        .enter()
-        .append('text')
-        .text(function(d, i){
-          return d.size;
+       .data(realQuakes)
+       .enter()
+       .append('text')
+       .text(function(d, i){
+        return d.size;
         })
-        .attr('x', function(d, i){
-          return i * (svgWidth / realQuakes.length) + (svgWidth / realQuakes.length - barPadding) / 2;
+       .attr('x', function(d, i){
+        return i * (svgWidth / realQuakes.length) + (svgWidth / realQuakes.length - barPadding) / 2;
         })
-        .attr('y', function(d, i){
-          return svgHeight - d.size * 80 + 10;
+       .attr('y', function(d, i){
+        return svgHeight - d.size * 80 + 10;
         })
-        .attr('text-anchor', 'middle')
-        .attr('font-family', 'sans-serif')
-        .attr('font-size', '10px')
-        .attr('fill', 'white');
+       .attr('text-anchor', 'middle')
+       .attr('font-family', 'sans-serif')
+       .attr('font-size', '10px')
+       .attr('fill', 'white');
 
     svg.append('text')
-        .text('Earthquakes by size over last 48 hours')
-        .attr('x', 20)
-        .attr('y', 100);
+       .text('Earthquakes by size over last 48 hours')
+       .attr('x', 20)
+       .attr('y', 100);
 
     var svgMap = d3.select('body')
-            .append('svg')
-            .attr('height', svgHeight)
-            .attr('width', svgWidth)
-            .attr('class', 'quake-map-canvas');
+                   .append('svg')
+                   .attr('height', svgHeight)
+                   .attr('width', svgWidth)
+                   .attr('class', 'quake-map-canvas');
 
     var xScale = d3.scale.linear()
-                  .domain([d3.min(realQuakes, function(d){
+                   .domain([d3.min(realQuakes, function(d){
                     return d.longitude;
                     }), d3.max(realQuakes, function(d){
                       return d.longitude;
                     })])
-                  .range([scatterPadding, svgWidth - scatterPadding * 4]);
+                   .range([scatterPadding, svgWidth - scatterPadding * 4]);
 
     var yScale = d3.scale.linear()
-                  .domain([d3.min(realQuakes, function(d){
+                   .domain([d3.min(realQuakes, function(d){
                     return d.latitude;
                     }), d3.max(realQuakes, function(d){
                       return d.latitude;
                     })])
-                  .range([svgHeight - scatterPadding, scatterPadding]);
+                   .range([svgHeight - scatterPadding, scatterPadding]);
 
     var rScale = d3.scale.linear()
-                    .domain([0, d3.max(realQuakes, function(d){
-                      return d.size;
+                   .domain([0, d3.max(realQuakes, function(d){
+                    return d.size;
                     })])
-                    .range([0, 10]);
+                   .range([0, 10]);
 
     svgMap.selectAll('circle')
-            .data(realQuakes)
-            .enter()
-            .append('circle')
-            .attr('cx', function(d){
-              return xScale(d.longitude);
-            })
-            .attr('cy', function(d){
-              return yScale(d.latitude);
-            })
-            .attr('r', function(d){
-              return rScale(d.size);
-            });
+          .data(realQuakes)
+          .enter()
+          .append('circle')
+          .attr('cx', function(d){
+            return xScale(d.longitude);
+          })
+          .attr('cy', function(d){
+            return yScale(d.latitude);
+          })
+          .attr('r', function(d){
+            return rScale(d.size);
+          });
 
     svgMap.selectAll('text')
-            .data(realQuakes)
-            .enter()
-            .append('text')
-            .text(function(d){
-              return d.longitude + ", " + d.latitude;
-            })
-            .attr('x', function(d){
-              return xScale(d.longitude);
-            })
-            .attr('y', function(d){
-              return yScale(d.latitude);
-            })
-            .attr('font-family', 'sans-serif')
-            .attr('font-size', '11px')
-            .attr('fill', 'red');
+          .data(realQuakes)
+          .enter()
+          .append('text')
+          .text(function(d){
+            return d.longitude + ", " + d.latitude;
+          })
+          .attr('x', function(d){
+            return xScale(d.longitude);
+          })
+          .attr('y', function(d){
+            return yScale(d.latitude);
+          })
+          .attr('font-family', 'sans-serif')
+          .attr('font-size', '11px')
+          .attr('fill', 'red');
 
     svgMap.append('text')
-        .text('Earthquake latitude & longitude with size')
-        .attr('x', 20)
-        .attr('y', 100);
+          .text('Earthquake latitude & longitude with size')
+          .attr('x', 20)
+          .attr('y', 100);
 
 }
