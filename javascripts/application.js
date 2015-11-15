58a01612e555e8d677c01cf5684547cd6bd8190a
alexrevell <revellalex@gmail.com>
2015-11-16 02:31:57 +1300

    refactor out most of promise body, add logError function to catch load error; separate out different graph rendering functions, change poll timer to 5 mins; make height of container css 100% so it goes to bottom of page;

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,4 +1,5 @@
 var IcelandQuakes = (function(){
+  var quakesDiv
   var svg
   var svgMap
   var svgHeight = 300
@@ -8,36 +9,35 @@ var IcelandQuakes = (function(){
   var currentQuakes
   var latestQuakes
   var quakesURL = "http://apis.is/earthquake/is"
-  var updateQuakesButton
 
   var bindEventListeners = function () {
-    // getAndExtractQuakesEventHandler(quakesURL, getQuakesJson, extractQuakes, renderQuakes)
     getInitialQuakes(quakesURL)
-    pollForQuakes()
+    pollForQuakes(quakesURL)
   }
 
-  function pollForQuakes(){
+  function pollForQuakes(url){
     setInterval( function(){
-      getAndDisplayQuakes(quakesURL)
-    }, 20000 )
+      getAndDisplayQuakes(url)
+    }, 300 * 1000 )
   }
 
   function getAndDisplayQuakes(url){
-    console.log("getAndDisplayQuakes()")
-    getQuakesJson(url).then(extractQuakes).then(checkForChange)//.then(assignCurrentQuakes).then(renderQuakes)
+    getQuakesJson(url).then(extractQuakes).then(checkForChange)
   }
 
   function getInitialQuakes(url){
-    console.log("getInitialQuakes()")
-    getQuakesJson(url).then(extractQuakes).then(assignCurrentQuakes).then(renderQuakes)
+    getQuakesJson(url).then(extractQuakes).then(assignCurrentQuakes).then(renderQuakes).catch(logError)
+  }
+
+  function logError(request) {
+    Error("Quake data didn't load successfully; error code:" + request.statusText)
   }
 
   function assignCurrentQuakes(quakes){
     return currentQuakes = quakes
   }
 
-  function checkForChange(quakes){
-    var latestQuakes = quakes
+  function checkForChange(latestQuakes){
     if (currentQuakes[0]['timestamp'] === latestQuakes[0]['timestamp']) {
       assignCurrentQuakes(latestQuakes)
       noChange(latestQuakes)
@@ -53,42 +53,18 @@ var IcelandQuakes = (function(){
   }
 
   function updateQuakesDisplay(quakes) {
-      console.log("new quakes have happened ! current, latest, quakes", currentQuakes, latestQuakes, quakes)
-      svg.remove()
-      svgMap.remove()
-      renderQuakes(quakes)
-      return quakes
+    console.log("new quakes have happened ! current:", currentQuakes, "new quakes: ", quakes)
+    svg.remove()
+    svgMap.remove()
+    renderLargeGraph(quakes)
+    renderSmallGraph(quakes)
+    renderQuakesMap(quakes)
+    return quakes
   }
 
-  // function getAndExtractQuakesEventHandler(url, getQuakesJson, extractQuakes, renderQuakes){
-    // getQuakesJson(url).then(updateQuakesDisplay)
-    // .then(function(quakes){
-    //   currentQuakes = extractQuakes(quakes)
-    //   renderQuakes(currentQuakes)
-    //   latestQuakes = currentQuakes
-    // }).catch(function(error){ console.log("error", error) })
-  // }
-
   function getQuakesJson(url) {
-    return new Promise(function (resolve, reject) {
-      var request = new XMLHttpRequest()
-      request.open('GET', url)
-      request.responseType = 'json'
-
-      request.onload = function() {
-        if (request.status === 200) {
-          resolve(request.response)
-        }
-        else {
-          reject(Error("Quake data didn't load successfully; error code:" + request.statusText));
-        }
-      }
-
-      request.onerror = function() {
-        reject(Error("Something erroreed like the network."))
-      }
-
-      request.send()
+    return new Promise( function(resolve, reject){
+      getQuakes(url, resolve)
     })
   }
 
@@ -104,106 +80,116 @@ var IcelandQuakes = (function(){
     return quake.size >= 0
   }
 
-  function renderQuakes(quakes){
+  function renderQuakes(quakes) {
+    renderLargeGraph(quakes)
+    renderSmallGraph(quakes)
+    renderQuakesMap(quakes)
+  }
 
-    d3.select('.quake-events').selectAll('div')
+  function renderSmallGraph(quakes) {
+    quakesDiv = d3.select('.quake-events').selectAll('div')
       .data(quakes)
       .enter()
       .append('div')
       .attr('class', 'bar')
       .style("background-color","red")
       .style('height', function(d){
-        return (d.size * 30) + 'px';
+        return (d.size * 30) + 'px'
       })
-      .style('width', '10px');
+      .style('width', '10px')
+  }
 
+  function renderLargeGraph(quakes) {
     svg = d3.select('.container-quakes')
             .append('svg')
             .attr('height', svgHeight)
             .attr('width', svgWidth)
-            .attr('class', 'quake-canvas');
+            .attr('class', 'quake-canvas')
 
     svg.selectAll('rect')
         .data(quakes)
         .enter()
         .append('rect')
         .attr('x', function(d, i){
-          return i * svgWidth / quakes.length;
+          return i * svgWidth / quakes.length
         })
         .attr('y', function(d){
-          return svgHeight - d.size * 80;
+          return svgHeight - d.size * 80
         })
         .attr('height', function(d){
-          return d.size * 80;
+          return d.size * 80
         })
         .attr('width', svgWidth / quakes.length - barPadding)
         .attr('fill', function(d){
-          return "rgb("+ (d.size * 120) +",0,0)";
-        });
+          return "rgb("+ (d.size * 120) +",0,0)"
+        })
 
     svg.selectAll('text')
        .data(quakes)
        .enter()
        .append('text')
        .text(function(d, i){
-          return d.size;
+          return d.size
         })
        .attr('x', function(d, i){
-          return i * (svgWidth / quakes.length) + (svgWidth / quakes.length - barPadding) / 2;
+          return i * (svgWidth / quakes.length) + (svgWidth / quakes.length - barPadding) / 2
         })
        .attr('y', function(d, i){
-          return svgHeight - d.size * 80 + 10;
+          return svgHeight - d.size * 80 + 10
         })
        .attr('text-anchor', 'middle')
        .attr('font-family', 'sans-serif')
        .attr('font-size', '10px')
-       .attr('fill', 'white');
+       .attr('fill', 'white')
 
     svg.append('text')
        .text('Earthquakes by size over last 48 hours')
        .attr('x', 20)
-       .attr('y', 100);
+       .attr('y', 100)
+  }
+
+  function renderQuakesMap(quakes){
 
     svgMap = d3.select('.container-quakes')
                .append('svg')
                .attr('height', svgHeight)
                .attr('width', svgWidth)
-               .attr('class', 'quake-map-canvas');
+               .attr('class', 'quake-map-canvas')
 
     var xScale = d3.scale.linear()
                    .domain([d3.min(quakes, function(d){
-                      return d.longitude;
+                      return d.longitude
                     }), d3.max(quakes, function(d){
-                      return d.longitude;
+                      return d.longitude
                     })])
-                   .range([scatterPadding, svgWidth - scatterPadding * 4]);
+                   .range([scatterPadding, svgWidth - scatterPadding * 4])
 
     var yScale = d3.scale.linear()
                    .domain([d3.min(quakes, function(d){
-                      return d.latitude;
+                      return d.latitude
                     }), d3.max(quakes, function(d){
-                      return d.latitude;
+                      return d.latitude
                     })])
-                   .range([svgHeight - scatterPadding, scatterPadding]);
+                   .range([svgHeight - scatterPadding, scatterPadding])
 
     var rScale = d3.scale.linear()
                    .domain([0, d3.max(quakes, function(d){
-                      return d.size;
+                      return d.size
                     })])
-                   .range([0, 10]);
+                   .range([0, 10])
 
     svgMap.selectAll('circle')
           .data(quakes)
           .enter()
           .append('circle')
           .attr('cx', function(d){
-            return xScale(d.longitude);
+            return xScale(d.longitude)
           })
           .attr('cy', function(d){
-            return yScale(d.latitude);
+            return yScale(d.latitude)
           })
           .attr('r', function(d){
-            return rScale(d.size);
+            return rScale(d.size)
           });
 
     svgMap.selectAll('text')
@@ -211,22 +197,22 @@ var IcelandQuakes = (function(){
           .enter()
           .append('text')
           .text(function(d){
-            return d.longitude + ", " + d.latitude;
+            return d.longitude + ", " + d.latitude
           })
           .attr('x', function(d){
-            return xScale(d.longitude);
+            return xScale(d.longitude)
           })
           .attr('y', function(d){
-            return yScale(d.latitude);
+            return yScale(d.latitude)
           })
           .attr('font-family', 'sans-serif')
           .attr('font-size', '11px')
-          .attr('fill', 'red');
+          .attr('fill', 'red')
 
     svgMap.append('text')
           .text('Earthquake latitude & longitude with size')
           .attr('x', 20)
-          .attr('y', 100);
+          .attr('y', 100)
   }
 
   return { bindEventListeners: bindEventListeners }
